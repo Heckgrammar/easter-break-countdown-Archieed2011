4c7f6763949fe86cec84389001bdc05cc20190e2
Archie Edmondson <22AEdmondson@heckgrammar.co.uk>
2025-04-04 10:15:03 +0100

    fuinished

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("What is your countdown?");
+            string countdown1 = Console.ReadLine();
+
+            Console.WriteLine("What date is your countdown?");
+            DateTime countdown = new Console.ReadLine();
+
+            DateTime current = DateTime.Now;
+
+            //DateTime target = new DateTime(2025, 4, 4, 15, 10, 5);
+
+            TimeSpan difference = countdown - current;
+            Console.WriteLine("There is " + difference.Days + " days, " + difference.Hours + " hours, " + difference.Minutes + " minutes, and " + difference.Seconds + " seconds until" + countdown1);
+           
+            Thread.Sleep(1000);
+
+            //if (difference.Days < 0)
+            //{
+              //  Console.WriteLine("It's the easter holidays");
+            //}
         }
     }
 }
